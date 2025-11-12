164ce0d0529b5af1f4b59853a1eacc19dc5a1c47
Ibragim <yandaroviba@mail.ru>
2025-11-12 10:56:05 +0300

    add protected routes

--- internal/api/server/server.go
+++ internal/api/server/server.go
@@ -3,21 +3,27 @@ package server
 import (
 	"net/http"
 
+	"github.com/avraam311/warehouse-control/internal/api/handlers/auth"
 	"github.com/avraam311/warehouse-control/internal/api/handlers/items"
 	"github.com/avraam311/warehouse-control/internal/api/middlewares"
 
+	"github.com/wb-go/wbf/config"
 	"github.com/wb-go/wbf/ginext"
 )
 
-func NewRouter(ginMode string, handlerItem *items.Handler) *ginext.Engine {
-	e := ginext.New(ginMode)
+func NewRouter(cfg *config.Config, handlerItem *items.Handler, handlerAuth *auth.Handler) *ginext.Engine {
+	e := ginext.New(cfg.GetString("server.gin_mode"))
 
 	e.Use(middlewares.CORSMiddleware())
 	e.Use(ginext.Logger())
 	e.Use(ginext.Recovery())
 
 	api := e.Group("/warehouse-control/api")
+	api.Use(middlewares.RoleBasedAuthMiddleware(cfg.GetString("auth.jwt_secret"), cfg.GetStringSlice("auth.allowed_roles")))
 	{
+		api.POST("/auth/register", handlerAuth.Register)
+		api.POST("/auth/login", handlerAuth.Login)
+
 		api.POST("/items", handlerItem.CreateItem)
 		api.GET("/items", handlerItem.GetItems)
 		api.PUT("/items/:id", handlerItem.PutItem)
